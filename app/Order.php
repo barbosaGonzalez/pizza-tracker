702b0e995994b351fae97f7ad22022068f16cfc4
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-10 19:54:07 +0800

    Setup order model relation

--- app/Order.php
+++ app/Order.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $fillable = ['user_id', 'address', 'size', 'toppings', 'instructions', 'status_id'];
+
+    public function customer()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function status()
+    {
+        return $this->belongsTo('App\Status');
+    }
 }
